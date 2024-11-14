51d39f68c38e2f2fca04cadff59356978a4524b1
Mukul Rai <mukul93028@gmail.com>
2024-11-14 17:39:42 +0530

    er2

--- app/shayari/page.tsx
+++ app/shayari/page.tsx
@@ -1,11 +1,6 @@
 'use client';
-// // import HomeContent from '@/components/HomeContent'
-// import { Suspense } from 'react';
+
 import React, { Suspense } from 'react'
-import { useSearchParams, useRouter } from 'next/navigation';
-// import QuotesList from '@/components/QuoteList';
-import CategoriesSidebar from '@/components/CategoriesSidebar';
-import ShayariList from '@/components/ShayariList';
 import ShayariContent from '@/components/ShayariContent';
 
 const Page = () => {
--- app/social_media_quotes/page.tsx
+++ app/social_media_quotes/page.tsx
@@ -2,25 +2,10 @@
 // import HomeContent from '@/components/HomeContent'
 import React from 'react'
 import { Suspense } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
-// import QuotesList from '@/components/QuoteList';
-// import CategoriesSidebar from '@/components/CategoriesSidebar';
-// import ShayariList from '@/components/ShayariList';
-import SocialMedia from '@/components/SocialMedia';
 import SocalMediaContent from '@/components/SocialMediaContent';
 
 const Page = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const currentPage = Number(searchParams.get('page')) || 1;
-  const currentCategory = searchParams.get('category') || 'all';
-
-  const updateQueryParams = (page: number, category: string) => {
-    const params = new URLSearchParams();
-    params.set('page', page.toString());
-    if (category !== 'all') params.set('category', category);
-    router.push(`/shayari/?${params.toString()}`);
-  };
+  
 
   return (
     <>
