20fa7950858706e36787a2b1a80baa8c96084997
Mukul Rai <mukul93028@gmail.com>
2024-11-18 23:58:16 +0530

    url error fix4

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,12 +1,33 @@
+// import React, { Suspense } from 'react';
+// import HomeContent from '@/components/HomeContent';
+
+// export default async function Page({ 
+//   params 
+// }: { 
+//   params: { category: string } 
+// }) {
+//   const currentCategory = params.category || 'all';
+
+//   return (
+//     <>
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <HomeContent para={currentCategory} />
+//       </Suspense>
+//     </>
+//   );
+// }
+
 import React, { Suspense } from 'react';
 import HomeContent from '@/components/HomeContent';
 
-export default async function Page({ 
-  params 
-}: { 
-  params: { category: string } 
-}) {
-  const currentCategory = params.category || 'all';
+type PageProps = {
+  params: {
+    category: Promise<string>;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
+  const currentCategory = await params.category || 'all';
 
   return (
     <>
