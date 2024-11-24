c79325f8d219df466db3ff970f7fcdab25565bdb
Mukul Rai <mukul93028@gmail.com>
2024-11-24 17:09:24 +0530

    sitemap.xml

---
+++ app/sitemap.ts
@@ -0,0 +1,168 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.purevichar.in/',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 1.00,
+    },
+    {
+      url: 'https://www.purevichar.in/shayari',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.95,
+    },
+    {
+      url: 'https://www.purevichar.in/social_media_quotes',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/all',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/inspirational-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/life-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/love-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/faith-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/success-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/happiness-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/motivational-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/wisdom-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/humor-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/philosophy-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/hope-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/romance-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/poetry-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/god-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/religion-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/spirituality-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/time-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/you-quotes-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/shayari/all',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/shayari/sad-shayari-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/shayari/love-shayari-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/shayari/funny-shayari-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    },
+    {
+      url: 'https://www.purevichar.in/shayari/heartbreak-shayari-hindi',
+      lastModified: new Date('2024-11-24T07:52:48+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.90,
+    }
+  ];
+}
