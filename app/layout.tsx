223a26184e315078f17b2c88993a75700cb85626
Mukul Rai <mukul93028@gmail.com>
2024-11-25 23:40:30 +0530

    pintrest

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
   title: "Quotes and Shayari in Hindi",
   description: "Discover a treasure trove of heartfelt Hindi Shayari and Quotes on love, life, motivation,poetry,politics and philosophy. Daily updated collection featuring inspirational thoughts, good morning messages, sad shayari, and timeless wisdom in Hindi. Explore 10,000+ handpicked quotes across emotions, share with loved ones, and find words that touch your soul. Your daily destination for shayari in hindi and meaningful quotes. Visit now for fresh content daily!",
   keywords: "quotes in hindi,Love Quotes in Hindi,shayari in hindi",
+  
 };
 
 export default function RootLayout({
@@ -28,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+    <meta name="p:domain_verify" content="8c2fc0c02cd41d55426a95764aa392bf"/>
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col items-center justify-center`}
         style={{ backgroundColor: "#ede7e1", color: "#4b281e" }} // Set background and text color
