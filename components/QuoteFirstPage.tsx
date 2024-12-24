adb3bb29ef359f23f8ae484ce71cf20495d135da
Mukul Rai <mukul93028@gmail.com>
2024-12-25 01:13:17 +0530

    SSR

--- components/QuoteFirstPage.tsx
+++ components/QuoteFirstPage.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 // import QuoteFirstPage from '@/components/QuoteFirstPage';
-import { useRouter } from 'next/dist/client/components/navigation';
+// import { useRouter } from 'next/dist/client/components/navigation';
 import { quotes } from '@/constants/quotes';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
