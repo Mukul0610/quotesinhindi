36456fd29be04f1b4bc2a335d630c257082c5bf9
Mukul Rai <mukul93028@gmail.com>
2025-01-16 20:12:13 +0530

    shayari

--- constants/shayariCategories.ts
+++ constants/shayariCategories.ts
@@ -3,7 +3,8 @@ export const categories = [
     { id: 'love-shayari-hindi', english_name: "Love Shayari", name: 'प्यार शायरी', count: 2813 },
     { id: 'funny-shayari-hindi', english_name: "Funny Shayari", name: 'मजेदार शायरी', count: 1934 },
     { id: 'heartbreak-shayari-hindi', english_name: "Heartbreak Shayari", name: 'दिल टूटने वाली शायरी', count: 1234 },
-    { id: 'maa-shayari-hindi', english_name: "Mother Shayari", name: 'माँ पर शायरी शायरी', count: 353 }
+    { id: 'maa-shayari-hindi', english_name: "Mother Shayari", name: 'माँ पर शायरी', count: 353 },
+    { id: 'motivational-shayari-hindi', english_name: "Motivational Shayari", name: 'प्रेरक शायरी', count: 198 }
   ];
 
   export function getAllShayariCategories() {
